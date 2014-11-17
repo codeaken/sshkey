6f74e6357bb017d238e92c1e9301e75c8a647420
codeaken <magnus@codeaken.com>
2014-11-17 19:03:08 +0100

    Can get the password previously used for decrypting the private key

--- src/Codeaken/SshKey/SshPrivateKey.php
+++ src/Codeaken/SshKey/SshPrivateKey.php
@@ -3,9 +3,13 @@ namespace Codeaken\SshKey;
 
 class SshPrivateKey extends SshKey
 {
+    private $password;
+
     public function __construct($keyData, $password = '')
     {
         parent::__construct();
+
+        $this->password = $password;
 
         if ( ! empty($password)) {
             $this->key->setPassword($password);
@@ -17,6 +21,16 @@ class SshPrivateKey extends SshKey
     public static function fromFile($filename, $password = '')
     {
         return new SshPrivateKey(SshKey::readFile($filename), $password);
+    }
+
+    public function hasPassword()
+    {
+        return !empty($this->password);
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
     }
 
     protected function getKeyType()
